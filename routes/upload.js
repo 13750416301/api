1f0a6de738f4b562a72f6df75e625bd7b59b6da6
QinYingchao <1063492964@qq.com>
2019-08-27 09:16:24 +0800

    update upload

--- routes/upload.js
+++ routes/upload.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var multer  = require('multer');
+var multer  = require('multer');//multer 是express官方推荐的文件上传中间件
 var path = require('path');
 // var upload = multer({ dest: 'uploads' })
 var storage = multer.diskStorage({
   destination: function(req, res, cb) {
-    cb(null, 'C:/Users/FOREVERBOBO/Downloads/My Documents/二级项目3/api/uploads');
+    //cb(null, 'C:/Users/FOREVERBOBO/Downloads/My Documents/二级项目3/api/uploads');
+    //cb(null,'D:/test1/destination');
+    cb(null,'C:/Program Files/Apache Software Foundation/Tomcat 8.5/webapps/ROOT/videoWebSite/image');
   },
   filename: function(req, file, cb) {
     var filenameArr = file.originalname.split('.')
